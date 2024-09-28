320437ae40fae7801788d2cdab7e989060b1a9bc
Kirmez <107321567+Kirmez@users.noreply.github.com>
2024-09-28 15:27:52 +0200

    fixed bugs #2

--- server/server.js
+++ server/server.js
@@ -1,7 +1,33 @@
-const express = require('express')
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const app = express()
+mongoose
+  .connect("mongodb+srv://kirillmezentsev02:season@cluster0.rr96b.mongodb.net/")
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
 
-mongodb+srv://kirillmezentsev02:<db_password>@cluster0.rr96b.mongodb.net/
+const app = express();
 
-season
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`))
